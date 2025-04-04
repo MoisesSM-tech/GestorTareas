bc5cd7a4211a0fb2d334f9660c9e1b8867247af9
Moises <moisessaez2005@hotmail.com>
2025-04-04 17:34:52 +0200

    Primer commit

---
+++ src/Tareas/Main.java
@@ -0,0 +1,12 @@
+package Tareas;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) { 
+		System.out.println("Bienvenido al Gestor de Tareas"); 
+		// Aquí se llamarán las funcionalidades implementadas por cada integrante 
+		}
+
+}
