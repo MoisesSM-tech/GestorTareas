f3af6264beee52b2922a709914463f57f137bc14
Daniell21 <danialtenirsanchez@gmail.com>
2025-04-04 19:27:27 +0200

    Funcionalidad agregar tareas implementada

---
+++ src/Tareas/GestorTareas.java
@@ -0,0 +1,22 @@
+package Tareas;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GestorTareas {
+	
+    private ArrayList<String> tareas = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void agregarTarea() {
+        System.out.println("Ingrese una nueva tarea:");
+        String tarea = scanner.nextLine();
+        tareas.add(tarea);
+        System.out.println("Tarea añadida correctamente.");
+    }
+
+    public List<String> getTareas() {
+        return tareas;
+    }
+}
